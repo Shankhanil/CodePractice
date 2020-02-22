b7ca346a0ed49932d2ace8b31250dbe0eebfb0a1
Shankhanil Ghosh <shankha.rik@gmail.com>
2020-02-22 19:29:14 +0530

    using neural network score 0.78

--- Kaggle code/Titanic - Machine learning from disaster/titanicpy/imputation.py
+++ Kaggle code/Titanic - Machine learning from disaster/titanicpy/imputation.py
@@ -3,10 +3,16 @@
 # data imputation methods
 
 from sklearn.linear_models import LinearRegression
-
+import pandas  as pd
+import numpy as np
 def byMean(df):
     _df = df.fillna( df.mean(), inplace = False)
     return _df
     
 def byRegression(df):
+    #separate empty cells as test
+    _testL = pd.DataFrame()
+    
+    for e in df:
+        if e['Age'] == np.nan()
     
