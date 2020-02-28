4a996ec58b3cde534c91823ee44d8496d01f70a0
Shankhanil Ghosh <shankha.rik@gmail.com>
2020-02-28 10:49:55 +0530

    simple hill climbing

--- AI assignments university/hillclimb.py
+++ AI assignments university/hillclimb.py
@@ -1,13 +1,30 @@
 """
 Q4 
-Implement Hill climbing for function optimization
+Implement simple Hill climbing for function optimization
 """
 import pandas as pd
-
+import random as r
 if __name__ == "__main__":
 
     data = pd.read_csv("input/hillclimb.csv")
     
-    
-    print(data.shape)
+    LEN = data.shape[0]
+    I = r.randint(0, LEN)
+    OPT = data.iloc[I]['f']
+    pos = data.iloc[I]['x1']
+    MAXepoch = 5
+    epoch = 0
+    print("Hill climbing starting at x1 = {} f = {}".format(pos, OPT))
+    for i in range(I, LEN):
+        if data.iloc[i]['f'] <= OPT and epoch < MAXepoch:
+            print("x = {}, f = {}".format(pos, OPT))
+            OPT = data.iloc[i]['f']
+            pos = data.iloc[i]['x1']
+            epoch = epoch + 1
+        elif data.iloc[i]['f'] <= OPT and epoch == MAXepoch:
+            break
+        elif data.iloc[i]['f'] < OPT:
+            epoch = 0
+            pass
+    print("OPTIMAL x = {}, f = {}".format(pos, OPT))
     
