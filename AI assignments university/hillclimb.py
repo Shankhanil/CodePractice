1cd14b1eb0192fec4d78eaf778dbc059352ae4b8
Shankhanil Ghosh <shankha.rik@gmail.com>
2020-02-28 10:30:03 +0530

    update

---
+++ AI assignments university/hillclimb.py
@@ -0,0 +1,13 @@
+"""
+Q4 
+Implement Hill climbing for function optimization
+"""
+import pandas as pd
+
+if __name__ == "__main__":
+
+    data = pd.read_csv("input/hillclimb.csv")
+    
+    
+    print(data.shape)
+    
