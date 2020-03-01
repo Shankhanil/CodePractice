010a94b6fd252df062b1dc405de1c5383cbec231
Shankhanil Ghosh <shankha.rik@gmail.com>
2020-03-01 20:44:07 +0530

    solved prob13

--- Project Euler/prob13.py
+++ Project Euler/prob13.py
@@ -8,14 +8,18 @@ def _sum(N):
     S = ""
     carry = 0
     for i in range(49, -1, -1):
-        sum = 0
+        presum = 0
         for j in range(len(N)):
-            sum = sum+ int(N[j][i])
-        S = str((sum + carry)%10)+ S
-        print("i  = {} pre_Sum = {} Carry = {} post_sum = {} S = {}".format(i, sum, carry, sum + carry, S))
-        carry = int(sum/10)
-        
-    return S
+            presum = presum+ int(N[j][i])
+        #S = str((sum + carry)%10)+ S
+        postsum = presum + carry
+        carry = int(postsum/10)
+        sum = postsum%10
+        S = str(sum) + S
+        print("i  = {} pre_Sum = {} post_sum = {} Carry = {}  sum = {} S = {}".format(i, presum, postsum, carry, sum, S))
+        #carry = int(sum/10)
+    #print("i  = {} pre_Sum = {} post_sum = {} Carry = {}  sum = {} S = {}".format(-1, 0, carry, carry, sum, S))
+    return str(carry) + S
 
 if __name__ == "__main__":
     f = open("input13.txt", "r")
